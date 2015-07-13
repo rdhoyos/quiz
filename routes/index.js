cb37935f859dcddbb049d935d1dbb4a7fab351a5
rdhoyos <ronaldavidhoyos@gmail.com>
2015-07-12 19:09:18 -0500

    Datos de los autores

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/quizes/autor',quizController.autor);
 
 module.exports = router;
