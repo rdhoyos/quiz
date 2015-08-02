f1f0206d9afca663fbd60c701295485214a4817f
rdhoyos <ronaldavidhoyos@gmail.com>
2015-08-02 15:52:32 -0500

    Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,7 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/autor',quizController.autor);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 module.exports = router;
